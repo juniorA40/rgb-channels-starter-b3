7c3fea4804b20a1a8e96d008b665facc6a3a1e4f
junior <jalvarado@wlapcs.org>
2018-01-23 20:37:25 +0000

    fixed image

--- sketch.js
+++ sketch.js
@@ -2,24 +2,45 @@ var lpkidsred;
 var lpkidsgreen;
 var lpkidsblue;
 var lpkidsgray;
+var Telly
 
 function preload(){
     lpkidsgray = loadImage("images/lpkids-gray.png");
     lpkidsred = loadImage("images/lpkids-red.png");
     lpkidsgreen = loadImage("images/lpkids-green.png");
     lpkidsblue = loadImage("images/lpkids-blue.png");
+    Telly = loadImage("images/old-tv.png");
     
 }
 
 function setup(){
-    createCanvas(500,374);
+    createCanvas(749,524);
 }
 
 function draw(){
     if(keyIsPressed){
-      image(lpkidsred,0,0);  
+        if(key=="r"){
+            image(lpkidsred, 50, 50);
+        }
+        if(key=="g"){
+            image(lpkidsgreen, 50, 50);
+        }
+        if(key=="b"){
+            image(lpkidsblue, 50, 50);
+        }
+        if(keyCode=="32"){
+            fill(0);
+            rect(50, 50, 500, 374);
+            blendMode(LIGHTEST);
+            image(lpkidsred,50,50);
+            image(lpkidsgreen,50,50);
+            image(lpkidsblue,50,50);
+        }
     }
     else{
-        image(lpkidsgray,0,0);
-    }        
+        blendMode(NORMAL);
+        image(lpkidsgray,50,50);
+        image(Telly,0,0);
+    }
 }
+
